831a2653e2b3347f9d257ab704ae2d0541f0feb8
Panji Kumbara <108613621+PanjiKumbara@users.noreply.github.com>
2025-08-11 17:03:29 +0700

    Update LearningJourneyARE_Log.sql
    Update: fix as (alias)

--- LearningJourneyARE_Log.sql
+++ LearningJourneyARE_Log.sql
@@ -5,10 +5,10 @@ WITH RankedQuiz AS (
         uu.username,
         qq.content_id,
         qq.quiz_id,
-        qq.title,
-        qq.code,
+        cc.title as course_title,
+        qq.code as quiz_type,
         qq.score,
-        qq.created,
+        qq.created as quiz_created,
         qq.is_pass,
         ROW_NUMBER() OVER (
             PARTITION BY uu.username, qq.quiz_id
@@ -19,6 +19,7 @@ WITH RankedQuiz AS (
         ) AS num_attempts
     FROM quizzes_quizsession qq
 	LEFT JOIN UsersMbeat uu on uu.id = qq.user_id
+	LEFT JOIN ContentsCourse2 cc on cc.id = qq.content_id
     WHERE qq.title LIKE '%Learning Journey ARE : %'
        OR qq.title LIKE '%Learning Journey SPV %'
 )
@@ -30,10 +31,10 @@ SELECT
     de.[Join Date],
     rq.content_id,
     rq.quiz_id,
-    rq.title,
-    rq.code,
+    rq.course_title,
+    rq.quiz_type,
     rq.score,
-    rq.created,
+    rq.quiz_created,
     rq.is_pass,
     CASE WHEN rq.is_pass = 1 THEN 'Lulus' ELSE 'Tidak Lulus' END                  AS [Status Kelulusan],
     CASE WHEN rq.is_pass = 1 OR rq.num_attempts >= 3 THEN 'Completed'
@@ -41,16 +42,16 @@ SELECT
     rq.num_attempts,
     -- Course Level
     CASE 
-        WHEN rq.title LIKE '%Learning Journey ARE : Intermediate Course Lv I %' THEN 'Intermediate Course Level 1'
-        WHEN rq.title LIKE '%Learning Journey ARE : Intermediate Course Lv II%' THEN 'Intermediate Course Level 2'
-        WHEN rq.title LIKE '%Learning Journey ARE : Advance Course Lv I %'      THEN 'Advance Course Level 1'
-        WHEN rq.title LIKE '%Learning Journey ARE : Advance Course Lv II%'      THEN 'Advance Course Level 2'
-        WHEN rq.title LIKE '%Learning Journey SPV Agency : Intermediate Course%' THEN 'Intermediate Course'
-        WHEN rq.title LIKE '%Learning Journey SPV Agency : Advance Course%'      THEN 'Advance Course'
+        WHEN rq.course_title LIKE '%Learning Journey ARE : Intermediate Course Lv I %' THEN 'Intermediate Course Level 1'
+        WHEN rq.course_title LIKE '%Learning Journey ARE : Intermediate Course Lv II%' THEN 'Intermediate Course Level 2'
+        WHEN rq.course_title LIKE '%Learning Journey ARE : Advance Course Lv I %'      THEN 'Advance Course Level 1'
+        WHEN rq.course_title LIKE '%Learning Journey ARE : Advance Course Lv II%'      THEN 'Advance Course Level 2'
+        WHEN rq.course_title LIKE '%Learning Journey SPV Agency : Intermediate Course%' THEN 'Intermediate Course'
+        WHEN rq.course_title LIKE '%Learning Journey SPV Agency : Advance Course%'      THEN 'Advance Course'
         ELSE 'Unknown Course Level'
     END AS [Course Level]
-INTO PBI_LearningJourneyARE_Log
+	INTO PBI_LearningJourneyARE_Log
 FROM RankedQuiz rq
 LEFT JOIN DataEmployeeMbeat de ON de.nik = rq.username
 WHERE rq.rn = 1   -- ambil skor tertinggi, jika sama ambil tanggal terawal
-ORDER BY rq.title ASC, rq.username ASC;
+ORDER BY rq.course_title ASC, rq.username ASC;
