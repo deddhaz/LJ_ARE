a9568a68f477bcf342d381be9623e3ec0f2b5e3f
Panji Kumbara <108613621+PanjiKumbara@users.noreply.github.com>
2026-04-02 09:33:30 +0700

    Update (Perubahan Nama Kolom)

--- LearningJourneyARE_DataTarget.sql
+++ LearningJourneyARE_DataTarget.sql
@@ -3,19 +3,18 @@ SELECT
     de.nik,
     de.Name,
     de.JobTitle,
-    de.WorkLocation,
+    de.worklocation,
     de.Area,
     de.Region,
-	de.Unit,
     de.[Join Date],
-    de.EmploymentStatus,
+    de.employmentstatus,
 	pg.[Status Kelulusan] as [Status Kelulusan GPOP],
 	pg1.[Status Kelulusan] as [Status Kelulusan GLP1],
     ph.[Handle Spv]
  
 INTO PBI_LearningJourneyARE_DataTarget
 FROM DataEmployeeMbeat de
-LEFT JOIN hcir.dbo.pichchandle ph ON ph.Cabang = de.WorkLocation
+LEFT JOIN hcir.dbo.pichchandle ph ON ph.Cabang = de.worklocation
 LEFT JOIN PBI_GPOP_Check pg on pg.NIK = de.nik
 left join PBI_GLP1_Cek pg1 on pg1.NIK = de.NIK
 Where de.JobTitle in ('ARE (Car Product)', 'ARE (Motorcycle Product)', 'Branch NDF Car Agency Supervisor', 'Branch Non Dealer Financing Motorcycle Spv Agency', 'Branch NDF Multiproduct Agency Supervisor') or de.NIK in ('076201', '5002479', '5002517', '5002530', '5002580')
